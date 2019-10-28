7ad2f9ef1ab9cca0363f10eda51f180767cc521e
AHorejsi <alex.horejsi59@gmail.com>
2019-10-28 13:51:30 -0700

    Updating MySQL database creation query

--- LogEmail/create.sql
+++ LogEmail/create.sql
@@ -5,8 +5,13 @@ USE db;
 CREATE TABLE IF NOT EXISTS Users (
 	UserId INT NOT NULL AUTO_INCREMENT,
 	Username VARCHAR(255) NOT NULL,
-	Password VARCHAR(255) NOT NULL,
 	Email VARCHAR(255) NOT NULL,
 	Dashboard JSON,
 	PRIMARY KEY (UserId)
 );
+
+INSERT INTO Users (Username, Email, Dashboard) VALUES ("AlexHorejsi", "alex.horejsi59@gmail.com", NULL);
+INSERT INTO Users (Username, Email, Dashboard) VALUES ("MisaelCorvera", "mcmalfaro140@gmail.com", NULL);
+INSERT INTO Users (Username, Email, Dashboard) VALUES ("YiWang", "superhotdogzz@gmail.com", NULL);
+INSERT INTO Users (Username, Email, Dashboard) VALUES ("ZacYou", "zacyou151@yahoo.com", NULL);
+INSERT INTO Users (Username, Email, Dashboard) VALUES ("JatDida", "didajateni@gmail.com", NULL);
