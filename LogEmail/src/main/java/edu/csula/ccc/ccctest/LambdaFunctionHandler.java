dc69693350d55a93b0f65920d58e4bac19716132
AHorejsi <alex.horejsi59@gmail.com>
2019-10-28 13:30:41 -0700

    Adding .gitignore

--- LogEmail/src/main/java/edu/csula/ccc/ccctest/LambdaFunctionHandler.java
+++ LogEmail/src/main/java/edu/csula/ccc/ccctest/LambdaFunctionHandler.java
@@ -1,6 +1,5 @@
 package edu.csula.ccc.ccctest;
 
-import java.util.Map;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
@@ -14,30 +13,22 @@ import com.amazonaws.services.sns.model.PublishRequest;
 import com.amazonaws.services.sns.model.PublishResult;
 
 public class LambdaFunctionHandler implements RequestHandler<Object, String> {
-	private static final String REGION = "";
-	private static final String TOPIC_ARN = "";
-	private static final String ACCESS_KEY_ID = "";
-	private static final String SECRET_ACCESS_KEY = "";
-	private static final AWSCredentials CREDENTIALS = new BasicAWSCredentials(ACCESS_KEY_ID, SECRET_ACCESS_KEY);
+	private static final AwsParams PARAMS = AwsParams.getInstance();
+	private static final AWSCredentials CREDENTIALS = new BasicAWSCredentials(PARAMS.getAccessKeyId(), PARAMS.getSecretAccessKey());
 	private static final AWSCredentialsProvider CREDENTIALS_PROVIDER = new AWSStaticCredentialsProvider(CREDENTIALS);
-	private static final AmazonSNS SNS_CLIENT = AmazonSNSClientBuilder.standard().withRegion(REGION).withCredentials(CREDENTIALS_PROVIDER).build();
+	private static final AmazonSNS SNS_CLIENT = AmazonSNSClientBuilder.standard().withRegion(PARAMS.getRegion()).withCredentials(CREDENTIALS_PROVIDER).build();
 	
 	@Override
     public String handleRequest(Object input, Context context) {
 		final String decodedInput = this.decodeInput(input);
-		final PublishRequest request = new PublishRequest(TOPIC_ARN, decodedInput);
+		final PublishRequest request = new PublishRequest(PARAMS.getTopicArn(), decodedInput);
 		final PublishResult result = SNS_CLIENT.publish(request);
 		
 		final LambdaLogger logger = context.getLogger();
 		logger.log("Message Id: " + result.getMessageId());
 		logger.log("Input: " + decodedInput);
 		
-		Map<String, ?> map = (Map<String, ?>)input;
-			
-		for (Map.Entry<String, ?> entry : map.entrySet()) {
-			logger.log(entry.getKey().getClass().toString());
-			logger.log(entry.getValue().getClass().toString());
-		}
+		//Map<String, ?> map = (Map<String, ?>)input;
 
         return "Hello from Lambda!";
     }
