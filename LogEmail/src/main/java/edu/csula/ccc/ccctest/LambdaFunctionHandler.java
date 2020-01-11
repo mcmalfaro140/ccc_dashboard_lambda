2b71841395697906ada2536e9fcece9bd95de40a
ALEX <alex.horejsi59@gmail.com>
2020-01-11 13:49:24 -0800

    Adding gitignore

--- LogEmail/src/main/java/edu/csula/ccc/ccctest/LambdaFunctionHandler.java
+++ LogEmail/src/main/java/edu/csula/ccc/ccctest/LambdaFunctionHandler.java
@@ -1,8 +1,16 @@
 package edu.csula.ccc.ccctest;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.GZIPInputStream;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
@@ -10,30 +18,63 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sns.model.PublishRequest;
-import com.amazonaws.services.sns.model.PublishResult;
 
 public class LambdaFunctionHandler implements RequestHandler<Object, String> {
-	private static final AwsParams PARAMS = AwsParams.getInstance();
-	private static final AWSCredentials CREDENTIALS = new BasicAWSCredentials(PARAMS.getAccessKeyId(), PARAMS.getSecretAccessKey());
-	private static final AWSCredentialsProvider CREDENTIALS_PROVIDER = new AWSStaticCredentialsProvider(CREDENTIALS);
-	private static final AmazonSNS SNS_CLIENT = AmazonSNSClientBuilder.standard().withRegion(PARAMS.getRegion()).withCredentials(CREDENTIALS_PROVIDER).build();
-	
 	@Override
     public String handleRequest(Object input, Context context) {
-		final String decodedInput = this.decodeInput(input);
-		final PublishRequest request = new PublishRequest(PARAMS.getTopicArn(), decodedInput);
-		final PublishResult result = SNS_CLIENT.publish(request);
-		
-		final LambdaLogger logger = context.getLogger();
-		logger.log("Message Id: " + result.getMessageId());
-		logger.log("Input: " + decodedInput);
-		
-		//Map<String, ?> map = (Map<String, ?>)input;
+		String decodedInput = this.decodeInput(input);
+		List<LogEvent> logEventList = LogEvent.extractLogEvents(decodedInput);
+		this.sendMessage(logEventList);
 
-        return "Hello from Lambda!";
+        return "";
     }
 	
 	private String decodeInput(Object input) {
-		return input.toString();
+		@SuppressWarnings("unchecked")
+		Map<String, Map<String, String>> map = (Map<String, Map<String, String>>)input;
+		String awsData = map.get("awslogs").get("data");
+		byte[] compressed = awsData.getBytes(StandardCharsets.US_ASCII);
+		byte[] decodedBytes = Base64.getDecoder().decode(compressed);
+		
+		try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(decodedBytes)) {
+			try (GZIPInputStream compressorInputStream = new GZIPInputStream(byteArrayInputStream)) {
+				try (InputStreamReader inputStreamReader = new InputStreamReader(compressorInputStream, StandardCharsets.US_ASCII)) {
+					try (BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+						StringBuilder output = new StringBuilder();
+						
+						for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
+							output.append(line);
+						}
+						
+						return output.toString();
+					}
+				}
+			}
+		} catch (IOException e) {
+			throw new InternalError("Failed to decompress content", e);
+		}
+	}
+	
+	private void sendMessage(List<LogEvent> logEventList) {
+		AWSCredentials credentials = new BasicAWSCredentials(AwsParams.ACCESS_KEY_ID, AwsParams.SECRET_ACCESS_KEY);
+		AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(credentials);
+		AmazonSNS snsClient = AmazonSNSClientBuilder
+				.standard()
+				.withRegion(AwsParams.REGION)
+				.withCredentials(credentialsProvider)
+				.build();
+		
+		for (LogEvent logEvent : logEventList) {
+			String level = logEvent.getLevel();
+			
+			if (level.equals("WARN")) {
+				PublishRequest request = new PublishRequest(AwsParams.WARN_TOPIC_ARN, logEvent.getStringRepresentation());
+				snsClient.publish(request);
+			}
+			else if (level.equals("ERROR")) {
+				PublishRequest request = new PublishRequest(AwsParams.ERROR_TOPIC_ARN, logEvent.getStringRepresentation());
+				snsClient.publish(request);
+			}
+		}
 	}
 }
--- LogEmail/src/test/java/edu/csula/ccc/ccctest/LambdaFunctionHandlerTest.java
+++ LogEmail/src/test/java/edu/csula/ccc/ccctest/LambdaFunctionHandlerTest.java
@@ -1,7 +1,8 @@
 package edu.csula.ccc.ccctest;
 
 import java.io.IOException;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -11,24 +12,23 @@ import com.amazonaws.services.lambda.runtime.Context;
  * A simple test harness for locally invoking your Lambda function handler.
  */
 public class LambdaFunctionHandlerTest {
-
     private static Object input;
 
     @BeforeClass
     public static void createInput() throws IOException {
-        // TODO: set up your sample input object here.
-    	LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
-    	map.put("key1", "value1");
-    	map.put("key2", "value2");
-    	map.put("key3", "value3");
+        // TODO: set up your sample input here.
+    	Map<String, Map<String, String>> map1 = new HashMap<String, Map<String, String>>();
+    	Map<String, String> submap1 = new HashMap<String, String>();
     	
-        input = map;
+    	submap1.put("data", "H4sIAAAAAAAAAM1QwWrcMBD9lUVn20iyZVm+mXSbQmkLuyE51CE49tQR2JKRtckuy/57xto0uQZCIAfBzJs372nekYwwz00PV4cJSEm+VVfV3a/1dltdrklE7JMBhzBjXDGWilzwAuHB9pfO7iaceJj9Gdl6B834Ar00EZl393Pr9OS1Nd/14MHNpPxLvNN9j9K3YXX9CMYv+JHoDhVSwaksCppmWa5kSosiU5nIU8kovlzmMuU0Z0hSQkkueZ4VClno5zUe5JsR/8aELKQUSkiq0uj/oSh/rN9oNSlrwimnMWUxYyvGS8HLVCa4U5OoJgM8whBY683mzyZg/gGv6wL44P0UG21jQQWNYQ9tzANn7FokLFZo9BMOgb3U182wg5qcFm27hBAmrR2Ttm2TZtJJa413dhjAJT/sCBev7Vn4fEfYqswKnLNu9QpGi5TxsPeB0MG/ZjdgfULDj6WrPjfdm2rz+4uFu3pqnNGmf1e6t6dnT3jSjkwDAAA=");
+    	map1.put("awslogs", submap1);
+    	
+        input = map1;
     }
 
     private Context createContext() {
         TestContext ctx = new TestContext();
 
-        // TODO: customize your context here if needed.
         ctx.setFunctionName("snsWithJava");
 
         return ctx;
@@ -39,9 +39,9 @@ public class LambdaFunctionHandlerTest {
         LambdaFunctionHandler handler = new LambdaFunctionHandler();
         Context ctx = createContext();
 
-        String output = handler.handleRequest(input, ctx);
+        String output1 = handler.handleRequest(input, ctx);
 
         // TODO: validate output here if needed.
-        Assert.assertEquals("Hello from Lambda!", output);
+        Assert.assertEquals("", output1);
     }
 }
