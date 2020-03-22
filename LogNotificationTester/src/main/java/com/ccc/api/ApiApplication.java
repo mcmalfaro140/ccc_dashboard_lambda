ca720f3cf4629adda74951537ada23444e9257d0
AHorejsi <alex.horejsi59@gmail.com>
2020-03-22 12:39:39 -0700

    Documented LogNotificationTester project and finished LAmbdaFunctionHandlerTest.java

--- LogNotificationTester/src/main/java/com/ccc/api/ApiApplication.java
+++ LogNotificationTester/src/main/java/com/ccc/api/ApiApplication.java
@@ -3,6 +3,10 @@ package com.ccc.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Starting point for the fake backend that tests
+ * the Lambda function
+ */
 @SpringBootApplication
 public class ApiApplication {
 	public static void main(String[] args) {
--- LogNotificationTester/src/main/java/com/ccc/api/controller/HomeController.java
+++ LogNotificationTester/src/main/java/com/ccc/api/controller/HomeController.java
@@ -7,10 +7,24 @@ import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The only controller containing the
+ * only endpoint for this fake backend
+ */
 @RestController
 public class HomeController {
+	/**
+	 * The logger that logs to the console and to
+	 * AWS CloudWatch
+	 */
     Logger logger = LoggerFactory.getLogger(HomeController.class);
-
+    
+    /**
+     * URL endpoint that logs to AWS CloudWatch
+     * Go to "localhost:5050/" to do logging
+     * @return An arbitrary string. Its value
+     * does not matter
+     */
     @GetMapping("/")
     public String hello() {
     	MDC.put("testKey", "testValue");
